8115e396e2d17d007f5b56de05942632c9b43d8c
NatGames <salavat-iskhakov@mail.ru>
2022-05-01 15:12:32 +0300

    add photo

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,16 +1,47 @@
 import React from 'react';
-import { Image, ScrollView, Text } from 'react-native';
+import { TouchableOpacity, Image, ScrollView, StyleSheet, Text } from 'react-native';
 
 const logo = {
-  uri: 'https://reactnative.dev/img/tiny_logo.png',
-  width: 64,
-  height: 64
+  uri: 'https://www.culture.ru/storage/images/4ed2740a-90c7-58d1-a244-092f28e7b7ac',
+  // width: 64,
+  // height: 64,
+
 };
 
 const HomeScreen = () => (
-  <ScrollView>
+  <ScrollView backgroundColor='#fff'>
+    <TouchableOpacity 
+      onPress={() => alert('This is the "Home" screen.')}
+      style={styles.container}
+      >
+        <Image 
+        source={logo}
+        style={styles.img}
+        // tintColor='grey'
+        // opacity= '.5'
+
+      />
+    </TouchableOpacity>
+    <TouchableOpacity 
+      onPress={() => alert('This is the "Home" screen.')}
+      style={styles.container}
+      >
+        <Image 
+        source={logo}
+        style={styles.img}
+      />
+    </TouchableOpacity>
+    <TouchableOpacity 
+      onPress={() => alert('This is the "Home" screen.')}
+      style={styles.container}
+      >
+        <Image 
+        source={logo}
+        style={styles.img}
+      />
+    </TouchableOpacity>
+
     <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
-    <Image source={logo} />
     <Image source={logo} />
     <Image source={logo} />
     <Image source={logo} />
@@ -44,3 +75,22 @@ const HomeScreen = () => (
 );
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+  img: {
+    width: 64,
+    height: 64,
+    borderColor: "red",
+    borderWidth: 5,
+    borderRadius: 50,
+    // opacity: 0.3
+    
+  }
+});
